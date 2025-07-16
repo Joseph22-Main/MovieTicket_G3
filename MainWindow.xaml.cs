b17bbd82a2e8aa6749b2db8ffd2f6c42f694f5bc
James Albert Uy <131651934+AkiRei091@users.noreply.github.com>
2025-07-16 17:26:49 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace main
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Recommended_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.ContentTemplate = (DataTemplate)FindResource("RecommendedView");
+            HighlightSidebar(RecommendedButton);
+        }
+
+        private void Movies_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.ContentTemplate = (DataTemplate)FindResource("MoviesView");
+            HighlightSidebar(MoviesButton);
+        }
+
+        private void Account_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.ContentTemplate = (DataTemplate)FindResource("AccountView");
+            HighlightSidebar(AccountButton);
+        }
+
+        private void HighlightSidebar(Button selectedButton)
+        {
+            RecommendedButton.Background = Brushes.Transparent;
+            MoviesButton.Background = Brushes.Transparent;
+            AccountButton.Background = Brushes.Transparent;
+
+            selectedButton.Background = new SolidColorBrush(Color.FromRgb(35, 54, 73)); // #233649
+        }
+    }
+}
