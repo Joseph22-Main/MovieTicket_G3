55bab0e9ffe9da56c53857f32ea5c309586262c5
nkogalang <nathangalang.college@gmail.com>
2025-07-21 06:36:06 +0800

    updated UI of admin dashboard (not yet finished)

--- AdminDashboard.xaml.cs
+++ AdminDashboard.xaml.cs
@@ -12,17 +12,71 @@ namespace MovieTicket_G3
 
         private void btnMovieUpdate_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new MovieUpdateControl();
+            AddButton.Visibility = Visibility.Visible;
+            DelButton.Visibility = Visibility.Visible;
+            EditButton.Visibility = Visibility.Visible;
         }
 
         private void btnSnackInventory_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new SnackInventoryControl();
+            AddSnack.Visibility = Visibility.Visible;
+            DelSnack.Visibility = Visibility.Visible;
+            EditSnack.Visibility = Visibility.Visible;
         }
 
         private void btnBookingMenu_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new BookingMenuControl();
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddSnack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DelSnack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditSnack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Movie1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Movie2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Movie3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Movie4_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
